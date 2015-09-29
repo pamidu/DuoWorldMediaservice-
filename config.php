995ff8fd8b731d739fe5665ad17a06ef21a32226
pamidu <pamidu@live.com>
2015-09-29 15:17:17 +0530

    media service duoworld

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	define("ROOT_PATH", "C:/wamp/www");
+	define("BASE_PATH", "C:/wamp/www/medialib");
+	define("STORAGE_PATH", BASE_PATH . "/media");
+	define("THUMB_PATH", BASE_PATH . "/thumbnails");
+	
+	define("SVC_OS_URL", "http://localhost:3000");
+	define("SVC_AUTH_URL", "http://dilshan.sossgrid.com:3048");
+?>
